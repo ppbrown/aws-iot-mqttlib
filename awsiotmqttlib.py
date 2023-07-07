f81177ba589c65d12d34a5b1065831c10ba98e0a
Philip Brown <phil@bolthole.com>
2023-07-07 01:34:51 +0000

    Added create_simple_iot_connection()

--- awsiotmqttlib.py
+++ awsiotmqttlib.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-
 # To use this code, either copy the file to your code directory,
 # and use
 #
@@ -64,12 +63,13 @@ def on_resubscribe_complete(resubscribe_future):
         if qos is None:
             sys.exit("Server rejected resubscribe to topic: {}".format(topic))
 
-
-
-def create_iot_connection(configfile="awsiotmqttlib-config.yaml"):
+def get_client_bootstrap():
   event_loop_group = io.EventLoopGroup(1)
   host_resolver = io.DefaultHostResolver(event_loop_group)
   client_bootstrap = io.ClientBootstrap(event_loop_group, host_resolver)
+  return client_bootstrap
+
+def create_iot_connection(configfile="awsiotmqttlib-config.yaml"):
 
   with open(configfile, 'r') as f:
     configdata = yaml.safe_load(f)
@@ -89,7 +89,7 @@ def create_iot_connection(configfile="awsiotmqttlib-config.yaml"):
     endpoint=endpoint,
     cert_filepath=cert_path,
     pri_key_filepath=key_path,
-    client_bootstrap=client_bootstrap,
+    client_bootstrap=get_client_bootstrap(),
     ca_filepath=root_ca_path,
     on_connection_interrupted=on_connection_interrupted,
     on_connection_resumed=on_connection_resumed,
@@ -99,9 +99,40 @@ def create_iot_connection(configfile="awsiotmqttlib-config.yaml"):
   )
   return mqtt_connection
 
-
 # To see result of connection clearly, use:
 # print("connect result=")
 # print (mqtt_connection.connect().result())
+
+
+
+########################################################
+# This "simple" version exists because sometimes, for unclear reasons,
+# the nicer fancier one above fails
+
+def create_simple_iot_connection(configfile="awsiotmqttlib-config.yaml"):
+
+  with open(configfile, 'r') as f:
+    configdata = yaml.safe_load(f)
+
+  thing_name   = configdata["config"]["thingName"]
+  cert_path    = configdata["config"]["certificateFilePath"]
+  key_path     = configdata["config"]["privateKeyPath"]
+  root_ca_path = configdata["config"]["rootCaPath"]
+  endpoint     = configdata["config"]["iotDataEndpoint"]
+
+  if not thing_name:
+    print("DEBUG: no thing_name set")
+    exit()
+
+  mqtt_connection = mqtt_connection_builder.mtls_from_path(
+    endpoint=endpoint,
+    client_id=thing_name,
+    cert_filepath=cert_path,
+    pri_key_filepath=key_path,
+    ca_filepath=root_ca_path,
+    client_bootstrap=get_client_bootstrap()
+  )
+  return mqtt_connection
+
 
 
